9ecfcafc05248eebc2e7522bbde33618ac13ad27
Pete Johnson <petecj2@gmail.com>
2019-07-22 18:06:12 -0400

    Paramaterized the OpenWeather API key

--- usage.py
+++ usage.py
@@ -1,4 +1,6 @@
 import samples101
+import sys
+
 print(samples101.reverse('hello'))
 print(samples101.starwars('hello'))
-print(samples101.weather('New Orleans', 'b6907d289e10d714a6e88b30761fae22'))
+print(samples101.weather('New Orleans', sys.argv[1]))
